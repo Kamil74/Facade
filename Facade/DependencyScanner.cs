2893f1868bc25c7d98f13fe8e6b1fec36ac388cb
Rafael Milewski <rafimilewski@hotmail.com>
2023-02-14 12:42:14 +0200

    Initial commit

---
+++ Facade/DependencyScanner.cs
@@ -0,0 +1,10 @@
+﻿namespace Facade;
+
+public class DependencyScanner
+{
+    public IEnumerable<string> DependencyScan(string githubUrl)
+    {
+        Console.WriteLine("Dependency scan");
+        return new List<string>() { "DependencyScan Error1" };
+    }
+}
---
+++ Facade/SecurityScanner.cs
@@ -0,0 +1,11 @@
+﻿namespace Facade;
+
+public class SecurityScanner
+{
+    public IEnumerable<string> SecurityScan(string githubUrl)
+    {
+        Console.WriteLine("Security Scan");
+
+        return new List<string>() { "security error1" };
+    }
+}
